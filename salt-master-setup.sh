c61b84ad1b494e3e726b376a25146a47c62e92b7
Deanna Thomposon <deanna.thompson45@outlook.com>
2024-02-21 20:52:35 -0500

    Reorder script

--- salt-master-setup.sh
+++ salt-master-setup.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 #Install Salt, Salt master service, and Salt minion service on RedHat VM
-
+#!/bin/bash
 mkdir /etc/apt/keyrings
 rpm --import https://repo.saltproject.io/salt/py3/redhat/9/x86_64/SALT-PROJECT-GPG-PUBKEY-2023.pub
 curl -fsSL https://repo.saltproject.io/salt/py3/redhat/9/x86_64/latest.repo | tee /etc/yum.repos.d/salt.repo
