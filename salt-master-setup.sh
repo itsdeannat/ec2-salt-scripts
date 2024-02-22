ab8a647f8aa90cd4defb6f85ee22f48926d16a22
Deanna Thomposon <deanna.thompson45@outlook.com>
2024-02-21 20:45:17 -0500

    Add master hostname and id

--- salt-master-setup.sh
+++ salt-master-setup.sh
@@ -4,12 +4,16 @@
 
 mkdir /etc/apt/keyrings
 rpm --import https://repo.saltproject.io/salt/py3/redhat/9/x86_64/SALT-PROJECT-GPG-PUBKEY-2023.pub
-curl -fsSL https://repo.saltproject.io/salt/py3/redhat/9/x86_64/latest.repo | sudo tee /etc/yum.repos.d/salt.repo
+curl -fsSL https://repo.saltproject.io/salt/py3/redhat/9/x86_64/latest.repo | tee /etc/yum.repos.d/salt.repo
 yum clean expire-cache
-yum install salt-master
-yum install salt-minion
+yum install -y salt-master
+yum install -y salt-minion
+rm -f /etc/salt/minion
+echo "master: 127.0.0.1" >> /etc/salt/minion
+echo "salt-master-01" > /etc/salt/minion_id 
 systemctl enable salt-master && systemctl start salt-master
 systemctl enable salt-minion && systemctl start salt-minion
+
 
 
 
