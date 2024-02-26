396f607da91242dee84efda011af379bb4ce6845
Deanna Thomposon <deanna.thompson45@outlook.com>
2024-02-26 17:09:21 -0500

    Update salt minion script

--- salt-minion-setup.sh
+++ salt-minion-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+mkdir /etc/apt/keyrings
 rpm --import https://repo.saltproject.io/salt/py3/redhat/9/x86_64/SALT-PROJECT-GPG-PUBKEY-2023.pub
-curl -fsSL https://repo.saltproject.io/salt/py3/redhat/9/x86_64/latest.repo | sudo tee /etc/yum.repos.d/salt.repo
+curl -fsSL https://repo.saltproject.io/salt/py3/redhat/9/x86_64/latest.repo | tee /etc/yum.repos.d/salt.repo
 yum clean expire-cache
-yum install salt-minion
+yum install -y salt-minion
 systemctl enable salt-minion && systemctl start salt-minion
